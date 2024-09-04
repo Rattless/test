1c7e2ae109f61a488e511da7ac589e20342d4dd7
Rattless <90244287+Rattless@users.noreply.github.com>
2024-09-04 00:24:50 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -125,7 +125,7 @@ do
         Title = "Dropdown",
         Values = horseList,
         Multi = true,
-        Default = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+        Default = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
     })
 
     local Slider = Tabs.Main:AddSlider("Slider", {
