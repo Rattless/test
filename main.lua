79e571a4ca56bb9b1bba59ee2e12ef58aadb901f
Rattless <90244287+Rattless@users.noreply.github.com>
2024-09-05 01:59:06 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,26 +1,20 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+local UserInputService = game:GetService("UserInputService")
 local Workspace = game:GetService("Workspace")
 local Player = game:GetService("Players").LocalPlayer
 
 horseList = {"Akhal-Teke", "Andalusian", "Appaloosa", "Arabian", "Clydesdale", "Dutch Warmblood", "Fjord", "Friesian", "Icelandic", "Marwari", "Mustang", "Paint Horse", "Percheron", "Quarter Horse", "Shire", "Thoroughbred"}
 
 horseEspTable = {}
-
-function checkHorse(item)
-    if item:GetAttributes() and item:GetAttribute("species") == "Horse" and not item:GetAttribute("owner") then
-        return true
-    else
-        return false
-    end
-end
+collectablesEspTable = {}
 
 function getList(all, type)
     local items = {}
     local Islands = Workspace.Islands
     local currentIsland = nil
-    
+
     for _, descendant in pairs(Islands:GetDescendants()) do
         if descendant.Name == Player.Name then
             currentIsland = descendant.Parent.Name
@@ -60,6 +54,35 @@ end
 --     print(item:GetAttribute("itemName"))
 -- end
 
+local function isCollectableDetected(insert)
+    if not insert:IsA("Model") then return false end
+    local itemName = insert:GetAttribute("itemName")
+    if itemName then
+        return insert
+    end
+
+    for _, child in ipairs(insert:GetChildren()) do
+        local childItemName = child:GetAttribute("itemName")
+        if childItemName then
+            return child
+        end
+    end
+
+    return false
+end
+
+local function isHorseDetected(insert)
+    if not insert:IsA("Model") then return false end
+    if not insert:FindFirstChild("OverheadPart") then return false end
+    local species = insert:GetAttribute("species")
+    local owner = insert:GetAttribute("owner")
+    local parent = insert.Parent
+    if species == "Horse" and not owner and parent.Name == "Animals" then
+        return true
+    end
+    return false
+end
+
 function esp(item, name, color)
     local text = Instance.new("BillboardGui")
 	text.Name = item.Name
@@ -76,12 +99,8 @@ function esp(item, name, color)
 	label.TextStrokeTransparency = 0
 	label.TextScaled = false
 	label.Parent = text
-    table.insert(horseEspTable, text)
+    return text
 end
-
--- for i,v in pairs(getHorses()) do
---     esp(v, v.OverheadPart.Overhead.BreedLabel.Text, Color3.new(1, 0, 0))
--- end
 
 local Window = Fluent:CreateWindow({
     Title = "Wild Horse Islands",
@@ -104,209 +123,141 @@ local Tabs = {
 local Options = Fluent.Options
 
 do
-    Fluent:Notify({
-        Title = "Notification",
-        Content = "This is a notification",
-        SubContent = "SubContent", -- Optional
-        Duration = 5 -- Set to nil to make the notification not disappear
+    -- Main
+    local NotifyHorse = Tabs.Main:AddToggle("Notify Horse Spawn", {
+        Title = "Notify Horse Spawn",
+        Description = "Notify when horses spawn",
+        Default = false
     })
-
-    Tabs.Main:AddButton({
-        Title = "Button",
-        Description = "Very important button",
-        Callback = function()
-            print("Button clicked!")
-        end
-    })
-
-    local NotifyHorse = Tabs.Main:AddToggle("Notify Horse Spawn", {Title = "Notify Horse Spawn", Default = false })
 
     local NotifyHorseList = Tabs.Main:AddDropdown("Horse List", {
-        Title = "Dropdown",
+        Title = "Notify List",
+        Description = "List of horses to notify",
         Values = horseList,
         Multi = true,
-        Default = {1, 2},
+        Default = {"Shire"}
     })
 
-    local Slider = Tabs.Main:AddSlider("Slider", {
-        Title = "Slider",
-        Description = "This is a slider",
-        Default = 2,
-        Min = 0,
-        Max = 5,
-        Rounding = 1,
-        Callback = function(Value)
-            print("Slider was changed:", Value)
+    -- Visuals
+    local HorseEsp = Tabs.Visuals:AddToggle("Horse Esp", {
+        Title = "Horse Esp",
+        Default = false
+    })
+
+    local CollectablesEsp = Tabs.Visuals:AddToggle("Collectable Esp", {
+        Title = "Collectable Esp",
+        Default = false
+    })
+
+    local HorseEspColor = Tabs.Visuals:AddColorpicker("Horse Colorpicker", {
+        Title = "Horse Esp Color",
+        Default = Color3.fromRGB(255, 255, 0)
+    })
+
+    local CollectablesEspColor = Tabs.Visuals:AddColorpicker("Collectable Colorpicker", {
+        Title = "Horse Esp Color",
+        Default = Color3.fromRGB(0, 255, 0)
+    })
+
+    -- Toggle Detection
+    HorseEsp:OnChanged(function()
+        if HorseEsp.Value then
+            for i, v in pairs(getList(false, "Animals")) do
+                local breedLabel = v.OverheadPart:FindFirstChild("Overhead") and v.OverheadPart.Overhead:FindFirstChild("BreedLabel")
+                local e = esp(v, breedLabel.Text,HorseEspColor.Value)
+                table.insert(horseEspTable, e)
+            end
+        else
+            for i = #horseEspTable, 1, -1 do
+                local v = horseEspTable[i]
+                if v then
+                    v:Destroy()
+                    table.remove(horseEspTable, i)
+                end
+            end
         end
-    })
-
-    Slider:OnChanged(function(Value)
-        print("Slider changed:", Value)
     end)
 
-    Slider:SetValue(3)
-
-    local Dropdown = Tabs.Main:AddDropdown("Dropdown", {
-        Title = "Dropdown",
-        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
-        Multi = false,
-        Default = 1,
-    })
-
-    Dropdown:SetValue("four")
-
-    Dropdown:OnChanged(function(Value)
-        print("Dropdown changed:", Value)
-    end)
-
-
-    
-    local MultiDropdown = Tabs.Main:AddDropdown("MultiDropdown", {
-        Title = "Dropdown",
-        Description = "You can select multiple values.",
-        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
-        Multi = true,
-        Default = {"seven", "twelve"},
-    })
-
-    MultiDropdown:SetValue({
-        three = true,
-        five = true,
-        seven = false
-    })
-
-    MultiDropdown:OnChanged(function(Value)
-        local Values = {}
-        for Value, State in next, Value do
-            table.insert(Values, Value)
+    CollectablesEsp:OnChanged(function()
+        if CollectablesEsp.Value then
+            for i, v in pairs(getList(false, "Collectables")) do
+                local item = isCollectableDetected(v)
+                local itemName = item:GetAttribute("itemName")
+                local e = esp(item, itemName, CollectablesEspColor.Value)
+                table.insert(collectablesEspTable, e)
+            end
+        else
+            for i = #collectablesEspTable, 1, -1 do
+                local v = collectablesEspTable[i]
+                if v then
+                    v:Destroy()
+                    table.remove(collectablesEspTable, i)
+                end
+            end
         end
-        print("Mutlidropdown changed:", table.concat(Values, ", "))
     end)
 
-
-
-    local Colorpicker = Tabs.Main:AddColorpicker("Colorpicker", {
-        Title = "Colorpicker",
-        Default = Color3.fromRGB(96, 205, 255)
-    })
-
-    Colorpicker:OnChanged(function()
-        print("Colorpicker changed:", Colorpicker.Value)
-    end)
-    
-    Colorpicker:SetValueRGB(Color3.fromRGB(0, 255, 140))
-
-
-
-    local TColorpicker = Tabs.Main:AddColorpicker("TransparencyColorpicker", {
-        Title = "Colorpicker",
-        Description = "but you can change the transparency.",
-        Transparency = 0,
-        Default = Color3.fromRGB(96, 205, 255)
-    })
-
-    TColorpicker:OnChanged(function()
-        print(
-            "TColorpicker changed:", TColorpicker.Value,
-            "Transparency:", TColorpicker.Transparency
-        )
-    end)
-
-
-
-    local Keybind = Tabs.Main:AddKeybind("Keybind", {
-        Title = "KeyBind",
-        Mode = "Toggle", -- Always, Toggle, Hold
-        Default = "LeftControl", -- String as the name of the keybind (MB1, MB2 for mouse buttons)
-
-        -- Occurs when the keybind is clicked, Value is `true`/`false`
-        Callback = function(Value)
-            print("Keybind clicked!", Value)
-        end,
-
-        -- Occurs when the keybind itself is changed, `New` is a KeyCode Enum OR a UserInputType Enum
-        ChangedCallback = function(New)
-            print("Keybind changed!", New)
+    HorseEspColor:OnChanged(function()
+        for _, v in pairs(horseEspTable) do
+            v:FindFirstChild("TextLabel").TextColor3 = HorseEspColor.Value
         end
-    })
-
-    -- OnClick is only fired when you press the keybind and the mode is Toggle
-    -- Otherwise, you will have to use Keybind:GetState()
-    Keybind:OnClick(function()
-        print("Keybind clicked:", Keybind:GetState())
     end)
 
-    Keybind:OnChanged(function()
-        print("Keybind changed:", Keybind.Value)
-    end)
+    -- Spawn Detection
+    Workspace.Islands.DescendantAdded:Connect(function(insert)
+        task.wait(0.1)
 
-    task.spawn(function()
-        while true do
-            wait(1)
+        -- Detect Horses
+        if isHorseDetected(insert) then
+            local breedLabel = insert.OverheadPart:FindFirstChild("Overhead") and insert.OverheadPart.Overhead:FindFirstChild("BreedLabel")
 
-            -- example for checking if a keybind is being pressed
-            local state = Keybind:GetState()
-            if state then
-                print("Keybind is being held down")
+            -- Nofify
+            if NotifyHorse.Value then
+                if table.find(NotifyHorseList.Values, breedLabel.Text) then
+                    Fluent:Notify({
+                        Title = "Horse Spawned",
+                        Content = breedLabel.Text,
+                        Duration = 5
+                    })
+                end
             end
 
-            if Fluent.Unloaded then break end
+            -- ESP
+            if HorseEsp.Value then
+                local e = esp(insert, breedLabel.Text, HorseEspColor.Value)
+                table.insert(horseEspTable, e)
+            end
+
         end
-    end)
 
-    Keybind:SetValue("MB2", "Toggle") -- Sets keybind to MB2, mode to Hold
+        -- Detect Collectables
+        if isCollectableDetected(insert) then
+            local item = isCollectableDetected(insert)
+            local itemName = item:GetAttribute("itemName")
 
-
-    local Input = Tabs.Main:AddInput("Input", {
-        Title = "Input",
-        Default = "Default",
-        Placeholder = "Placeholder",
-        Numeric = false, -- Only allows numbers
-        Finished = false, -- Only calls callback when you press enter
-        Callback = function(Value)
-            print("Input changed:", Value)
+            -- ESP
+            if CollectablesEsp.Value then
+                local e = esp(item, itemName, CollectablesEspColor.Value)
+                table.insert(collectablesEspTable, e)
+            end
         end
-    })
 
-    Input:OnChanged(function()
-        print("Input updated:", Input.Value)
     end)
 end
 
-
--- Addons:
--- SaveManager (Allows you to have a configuration system)
--- InterfaceManager (Allows you to have a interface managment system)
-
--- Hand the library over to our managers
 SaveManager:SetLibrary(Fluent)
 InterfaceManager:SetLibrary(Fluent)
-
--- Ignore keys that are used by ThemeManager.
--- (we dont want configs to save themes, do we?)
 SaveManager:IgnoreThemeSettings()
-
--- You can add indexes of elements the save manager should ignore
-SaveManager:SetIgnoreIndexes({})
-
--- use case for doing it this way:
--- a script hub could have themes in a global folder
--- and game configs in a separate folder per game
 InterfaceManager:SetFolder("FluentScriptHub")
-SaveManager:SetFolder("FluentScriptHub/specific-game")
+SaveManager:SetFolder("FluentScriptHub/wild-horse-islands-rattles")
 
 InterfaceManager:BuildInterfaceSection(Tabs.Settings)
 SaveManager:BuildConfigSection(Tabs.Settings)
 
-
 Window:SelectTab(1)
 
 Fluent:Notify({
-    Title = "Fluent",
+    Title = "Wild Horse Islands",
     Content = "The script has been loaded.",
-    Duration = 8
+    Duration = 5
 })
-
--- You can use the SaveManager:LoadAutoloadConfig() to load a config
--- which has been marked to be one that auto loads!
-SaveManager:LoadAutoloadConfig()
